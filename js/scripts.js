ebaa58550ffde1c8ba410c08e0612c1ee35c61ea
timpdotd <davidetimperi.01@gmail.com>
2023-10-18 11:01:03 +0200

    ok, the header and the sidebar seems finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector(".sidebar");
-
     sidebar.style.display = 'flex';
-    sidebar.style.width = "250px";
-}   
+    
+    if (window.innerWidth < 400) { // Adjust the screen width threshold as needed
+        sidebar.style.width = "100%";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
 
-function hideSidebar(){
+function hideSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.width = "0";
-
-}   
+}
