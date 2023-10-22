a240d492e47eddfcddd1b675b124770ece94cf3a
timpdotd <davidetimperi.01@gmail.com>
2023-10-22 23:27:25 +0200

    add favicon

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function showSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = 'flex';
     
-    if (window.innerWidth < 400) { // Adjust the screen width threshold as needed
+    if (window.innerWidth < 450) { // Adjust the screen width threshold as needed
         sidebar.style.width = "100%";
     } else {
         sidebar.style.width = "250px";
